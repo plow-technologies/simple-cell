7058643a69fce830b685263d7a54ce11d566aef4
Scott Murphy <scottmurphy09@gmail.com>
2014-10-11 23:20:46 -0500

    Moved all non-test functions into TestImport so I could easily call them from Bench for mocks.

--- test/SimpleStore/CellSpec.hs
+++ test/SimpleStore/CellSpec.hs
@@ -1,10 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 module SimpleStore.CellSpec (main, spec) where
 
-import Control.Applicative ((<$>))
-import Control.Monad
-import Data.Maybe (catMaybes)
-import Data.Traversable
+
+
 import SimpleStore
 import Test.Hspec
 import Test.QuickCheck (arbitrary, Property)
@@ -20,18 +18,6 @@ spec = do
     it "after initializing a value , closing and opening the value should match" $ do
       propInitThenRead
 
-
-runRestartTest :: [Int] -> IO [Int]
-runRestartTest i = do
-  let sis = Sample <$> i
-  sc <- initializeSampleSC "testSampleCell"
-  void $ traverse (insertSampleSC sc ) sis
-  createCheckpointAndCloseSampleSC sc
-  sc' <- initializeSampleSC "testSampleCell"
-  storeSamples <- traverse (getSampleSC sc') sis
-  samples <- traverse (traverse getSimpleStore) storeSamples
-  return $ sampleInt <$> (catMaybes samples)
- 
 
 
 propInitThenRead :: Property
