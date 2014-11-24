1c79ebced8711b3b0525b73913b44e3cdd196a9e
Scott Murphy <scottmurphy09@gmail.com>
2014-11-23 20:39:31 -0600

    Staging bench file

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-
+{-# LANGUAGE BangPatterns #-}
 import Control.Applicative ((<$>))
 import Control.Monad (void,forever)
 import qualified Data.Text as T
@@ -13,15 +13,17 @@ main :: IO ()
 main = do
   sc <- initializeSampleSC "benchTestCell"
   stdGen <- getStdGen 
-  let sis = take 500 $ Sample <$> randoms stdGen
+  let sis = take 5000 $ Sample <$> randoms stdGen
   void $ traverse (insertSampleSC sc) sis
   forever $ do
     void $ traverse (storeState sc) sis
-    threadDelay $ 60*1000*1000
 
 
 
-storeState sc st' = do 
+
+storeState sc st' = do
+  putStrLn "running"
+  threadDelay $ 3*1000  
   mSt <- getSampleSC sc st'
   case mSt of
     Nothing -> return st'
