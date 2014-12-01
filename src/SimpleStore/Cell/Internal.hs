41db2a1c2d1af332c543db9ee2924e32a818b8bd
Scott Murphy <scottmurphy09@gmail.com>
2014-11-30 22:11:20 -0600

    Added an stm fold to IO

--- src/SimpleStore/Cell/Internal.hs
+++ src/SimpleStore/Cell/Internal.hs
@@ -1,3 +1,35 @@
+{-# LANGUAGE BangPatterns #-}
+
+
 module SimpleStore.Cell.Internal
     (
+    ioFoldRListT
     ) where
+
+
+import           Control.Concurrent.STM
+import qualified ListT
+
+
+ioFoldRListT :: ListT.MonadTransUncons t =>
+       (a -> b -> b) -> b -> t STM a -> IO b
+ioFoldRListT fcn !seed lst = ioFoldRListT' fcn (return seed) lst 
+
+
+
+ioFoldRListT'
+  :: ListT.MonadTransUncons t =>
+     (a -> b -> b) -> IO b -> t STM a -> IO b
+ioFoldRListT' fcn !iseed lst = do 
+  (ma,mlst) <- atomically $ do
+                   ma <- ListT.head lst 
+                   mlst <- ListT.tail lst
+                   return (ma,mlst)
+  seed <- iseed                 
+  case ma of
+   Nothing -> return seed
+   Just val ->  do
+     maybe (return seed)
+           (ioFoldRListT' fcn (return $ fcn val seed))
+           mlst                 
+
